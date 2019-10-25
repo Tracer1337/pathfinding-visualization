09dd2f0e26f4b025cd079af2b799cf97c68cd532
Merlin Moelter <joebau.0815@googlemail.com>
2019-10-25 20:17:53 +0200

    Create build for github pages

---
+++ build/precache-manifest.5c0dfc2b4dfe8806226a7290bcb4cbe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512c90d5059c02533406c92b7068e568",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2175aed4fec73940256b",
+    "url": "/static/css/main.9210b9f7.chunk.css"
+  },
+  {
+    "revision": "52944186131a54a1fc7a",
+    "url": "/static/js/2.b4685032.chunk.js"
+  },
+  {
+    "revision": "2175aed4fec73940256b",
+    "url": "/static/js/main.e8e5c69f.chunk.js"
+  },
+  {
+    "revision": "f2e1c41cf403d8254738",
+    "url": "/static/js/runtime-main.ceba601a.js"
+  },
+  {
+    "revision": "9212f4767bc3b3321288fd063e9f8015",
+    "url": "/static/media/chevron_right-24px.9212f476.svg"
+  },
+  {
+    "revision": "81471c4b6d428b8577136a95aff9f637",
+    "url": "/static/media/highlight_off-24px.81471c4b.svg"
+  }
+]);
